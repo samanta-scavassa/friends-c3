648e803f27eacccae1f0cb37c54ae2d451fccf17
Samanta Scavassa <samanta.camargoterra@gmail.com>
2020-11-08 15:55:44 -0300

    Eureka config

--- src/main/java/com/c3/amigos/AmigosApplication.java
+++ src/main/java/com/c3/amigos/AmigosApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableFeignClients
+@EnableEurekaClient
 public class AmigosApplication {
 
 	@Bean
