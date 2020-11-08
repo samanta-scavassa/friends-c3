ff72f42763977de28a9e6d0dcf28923010d190d7
Samanta Scavassa <samanta.camargoterra@gmail.com>
2020-11-08 16:53:48 -0300

    Eureka config

--- src/main/java/com/c3/amigos/AmigosApplication.java
+++ src/main/java/com/c3/amigos/AmigosApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
 public class AmigosApplication {
